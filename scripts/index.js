a7cf316cb23c3ee160df4621767ef45059e26a6e
Denis Mogilevich <den-mogilevich@yandex.ru>
2022-08-27 16:14:59 +0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -5,22 +5,22 @@ let closeButton = document.querySelector('.popup__close-button');
 let title = document.querySelector('.profile__title');
 let about = document.querySelector('.profile__subtitle');
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.name');
-let aboutInput = document.querySelector('.about');
-function open() {
+let nameInput = document.querySelector('.popup__form-input_name');
+let aboutInput = document.querySelector('.popup__form-input_about');
+function openPopup() {
   popup.classList.add('popup_opened');
   nameInput.value = title.textContent;
   aboutInput.value = about.textContent;
 }
-function close() {
+function closePopup() {
   popup.classList.remove('popup_opened');
 }
 function formSubmitHandler (event) {
     event.preventDefault();
     title.textContent = nameInput.value;
     about.textContent = aboutInput.value;
-    close();
+    closePopup();
 }
 formElement.addEventListener('submit', formSubmitHandler);
-editButton.addEventListener('click', open);
-closeButton.addEventListener('click', close);
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
